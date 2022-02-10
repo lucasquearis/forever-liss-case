103beaeac711ca791bb6a32867c3766bbdca6d79
lucasquearis <lucasquearis@hotmail.com>
2022-02-10 19:24:18 -0300

    altera key em map das redes sociais para tirar warning

--- src/components/SocialMedias/index.jsx
+++ src/components/SocialMedias/index.jsx
@@ -9,7 +9,7 @@ function SocialMedias() {
     <div className="social-containers">
       <h1 className="title-social">VISITE NOSSAS REDES SOCIAIS</h1>
       <div className="social-medias">
-        {listSocialMedias.map((item) => <a target="_blank" href={item.url} rel="noreferrer"><img key={item.name} width="55px" src={item.image} alt={item.name} /></a>)}
+        {listSocialMedias.map((item) => <a key={item.name} target="_blank" href={item.url} rel="noreferrer"><img width="55px" src={item.image} alt={item.name} /></a>)}
       </div>
       <div className="social-nav">
         {listNavigation.map((item) => <p key={item}>{item}</p>)}
